cad75461efd46a08ba2d94cce1fbc34bb7e6b44b
Nov Hun <suybrake55@gmail.com>
2026-08-28 20:59:24 +0700

    chore: update CORS allowed origin to connect2hub.vercel.app

--- app/core/config.py
+++ app/core/config.py
@@ -25,7 +25,7 @@ class Settings(BaseSettings):
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:5173",
-        "https://connect-hub-web-flame.vercel.app",
+        "https://connect2hub.vercel.app",
         "*"
     ]
 
